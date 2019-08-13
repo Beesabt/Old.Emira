f213b1d981cfbe4431c2cbd0fb706886ec850931
Eszter Benkovics <Beesabt@github.com>
2019-08-14 00:56:50 +0200

    [Emira] - Add logger, refactor Holiday

---
+++ emiraTests/BusinessLogicLayer/LoginTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace emiraTests.BusinessLogicLayer.Tests
+{
+    [TestClass()]
+    public class LoginTests
+    {
+        [TestMethod()]
+        public void LoginValidationTest()
+        {
+            //var _login = new Login();
+            //Assert.IsFalse(_login.LoginValidation("cica", "mica"));
+        }
+    }
+}
